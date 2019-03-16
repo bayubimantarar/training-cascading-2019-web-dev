129896e64dfb8f732a825f846f64a4e5bfdf57eb
Bayu Bimantara <bayubimantarar@gmail.com>
2019-03-16 08:50:47 +0700

    Add cart total price function

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,12 @@
 window.onload = function() {
    var addtocartbtns = document.getElementsByClassName("addtocartbutton");
    var shoppingcart = document.getElementById("shoppingcart");
-   var cartitem = document.querySelectorAll("#shoppingcart ul")[0];
+   var cartitem = document.querySelectorAll("#shoppingcart ol")[0];
    var emptyCart = document.getElementById("emptyCart");
+   var temp_total = 0;
+   var total = 0;
+   var total_real_price = document.getElementsByClassName("total-real-price");
+   var div_total = document.createElement("div");
 
    currentCart = JSON.parse(localStorage.getItem('cart'));
    
@@ -18,7 +22,8 @@ window.onload = function() {
        addtocartbtns[i].addEventListener("click", function (ev) {
            var furnitureId = this.getAttribute("data-item");
            var furniturePrice = this.getAttribute("data-price");
-           var item = {id:furnitureId, price:furniturePrice};
+           var furnitureRealPrice = this.getAttribute("data-real-price");
+           var item = {id:furnitureId, price:furniturePrice, realPrice:furnitureRealPrice};
 
            currentCart.push(item);
            UpdateShoppingCartUI();
@@ -31,17 +36,30 @@ window.onload = function() {
         for (var i = 0; i < currentCart.length; i++) {
             var liElement = document.createElement('li');
             liElement.innerHTML = 
-            currentCart[i].id + " " + currentCart[i].price + "";
+            currentCart[i].id + " " + new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(currentCart[i].realPrice) + "";
             cartitem.appendChild(liElement);
+            total = parseInt(temp_total) + parseInt(currentCart[i].realPrice);
+        }
+
+        if(!div_total){
+          // document.getElementById("total").appendChild(div_total); 
+        }else{
+            temp_total = total;
+            div_total.innerHTML = '<p> ============= +</p>'+
+              '<p>Total '+new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(total)+'</p>';
+            document.getElementById("total").appendChild(div_total);
         }
 
         // --- perhatikan bagian ini -->
         localStorage.setItem('cart', JSON.stringify(currentCart));
     }
 
-    emptyCart.addEventListener("click", function (ev) {
+    if(emptyCart) {
+      emptyCart.addEventListener("click", function (ev) {
         currentCart = new Array();
-
+        temp_total = 0;
+        total = 0;
         UpdateShoppingCartUI();
-    });
+      }); 
+    }
 }
