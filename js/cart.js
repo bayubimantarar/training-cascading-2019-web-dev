6e6977027fa92dd266761d6b0791be40e172b0d7
Bayu Bimantara <bayubimantarar@gmail.com>
2019-03-17 08:04:18 +0700

    Add kelompok kerja picture and fixing cart module in all page

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,7 @@ window.onload = function() {
    var cartitem = document.querySelectorAll("#shoppingcart ol")[0];
    var emptyCart = document.getElementById("emptyCart");
    var temp_total = 0;
+   var total_price = 0;
    var total = 0;
    var total_real_price = document.getElementsByClassName("total-real-price");
    var div_total = document.createElement("div");
@@ -15,8 +16,6 @@ window.onload = function() {
    }
 
    UpdateShoppingCartUI();
-
-   var currentCart = new Array();
 
    for (var i = 0; i < addtocartbtns.length; i++) {
        addtocartbtns[i].addEventListener("click", function (ev) {
@@ -31,25 +30,32 @@ window.onload = function() {
     }
 
     function UpdateShoppingCartUI() {
-        cartitem.innerHTML = "<p>";
+        cartitem.innerHTML = "";
+        var totalBaru = 0;
 
-        for (var i = 0; i < currentCart.length; i++) {
+        for (var i=0; i<currentCart.length; i++) {
             var liElement = document.createElement('li');
-            liElement.innerHTML =
-            currentCart[i].id + " " + new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(currentCart[i].realPrice) + "";
-              cartitem.appendChild(liElement);
+            liElement.innerHTML = currentCart[i].id + " " + new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(currentCart[i].realPrice) + "";
+            cartitem.appendChild(liElement);
             total = parseInt(temp_total) + parseInt(currentCart[i].realPrice);
+            totalBaru += parseInt(currentCart[i].realPrice);
+            console.log(currentCart[i].realPrice)
         }
+
+        temp_total = total;
+        localStorage.setItem('total_price', totalBaru);
+        total_price = localStorage.getItem('total_price', total);
+        localStorage.setItem('real_price', total_price);
 
         if(!div_total){
           // document.getElementById("total").appendChild(div_total);
         }else{
             temp_total = total;
             div_total.innerHTML = '<p> ============= +</p>'+
-              '<p>Total '+new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(total)+'</p>';
+              '<p>Total '+new Intl.NumberFormat(['ban', 'id'], { style: 'currency', currency: 'IDR' }).format(localStorage.getItem('total_price'))+'</p>';
             document.getElementById("total").appendChild(div_total);
         }
-
+        console.log(currentCart);
         // --- perhatikan bagian ini -->
         localStorage.setItem('cart', JSON.stringify(currentCart));
     }
@@ -59,6 +65,8 @@ window.onload = function() {
         currentCart = new Array();
         temp_total = 0;
         total = 0;
+        localStorage.setItem('total_price', total);
+
         UpdateShoppingCartUI();
       });
     }
